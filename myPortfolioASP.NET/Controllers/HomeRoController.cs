1583fffe627353bbfde62690eb9909ca27de699e
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-08-04 15:45:59 +0300

    add HomeRoController and HomeRo pages

---
+++ myPortfolioASP.NET/Controllers/HomeRoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myPortfolioASP.NET.Controllers
+{
+    public class HomeRoController : Controller
+    {
+        private readonly ILogger<HomeRoController> _logger;
+
+        public IActionResult IndexRo()
+        {
+            return View();
+        }
+        public IActionResult AboutRo()
+        {
+            return View();
+        }
+        public IActionResult ContactRo()
+        {
+            return View();
+        }
+    }
+}
