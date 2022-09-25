131ef2a30a89fb31ad3e3e251a81444ffdf81cc8
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-09-25 13:09:50 +0300

    contact form
    not working

---
+++ wwwroot/js/scriptEmail.js
@@ -0,0 +1,11 @@
+﻿function SendEmail() {
+    var params = {
+        from_name: document.getElementById("name").value,
+        email_id: document.getElementById("email_id").value,
+        phone_id: document.getElementById("phone_id").value,
+        message: document.getElementById("message").value
+    }
+    emailjs.send("service_6c8qduj", "template_7lmpjdf", params).then
+        alert("Your message was sent!");
+    
+}
